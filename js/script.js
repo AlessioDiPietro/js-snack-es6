dd4ece8b78f770fe24425a8a8a0c89f25bf8b21c
AlessioDPT <alessio.recos@gmail.com>
2021-06-16 17:20:05 +0200

    ADD-es_2

--- js/script.js
+++ js/script.js
@@ -31,7 +31,6 @@ for (let i = 1; i < negozioBici.length; i++){
 };
 
 const {nome,peso} = biciLight;
-console.log(nome,peso);
 
 document.getElementById("es_1").innerHTML= 
 `   
@@ -44,3 +43,49 @@ document.getElementById("es_1").innerHTML=
 
 
 // Snack2 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. (Utilizzate le arrow function). Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti che contengono solo nomi e falli subiti. Stampiamo tutto in console.
+
+
+const numRand = (min, max) => Math.floor(Math.random()*(max - min + 1) + min);
+
+
+
+const serieA = [
+    {
+        nomeSquadra: "Albinoleffe",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nomeSquadra: "Lecce",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nomeSquadra: "Novara",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nomeSquadra: "Pro Vercelli",
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+];
+
+let campionato = [];
+
+
+for (let i = 0; i < serieA.length; i++){
+
+    serieA[i].puntiFatti = numRand(0, 10);
+    serieA[i].falliSubiti = numRand(0, 20);
+
+    campionato.push(serieA[i].nomeSquadra);
+    campionato.push(serieA[i].falliSubiti);
+
+}
+
+const {nomeSquadra,puntiFatti,falliSubiti} = campionato;
+
+console.log(campionato);
+
